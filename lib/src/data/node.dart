7c22808efe291646b2dafb6cca14ba5cf2e2c6ad
Boris Kaul <localvoid@gmail.com>
2015-03-18 17:45:13 +0600

    Add DataNode classes

---
+++ lib/src/data/node.dart
@@ -0,0 +1,87 @@
+// Copyright (c) 2015, the uix project authors. Please see the AUTHORS file for
+// details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+library uix.src.data.node;
+
+import '../env.dart';
+
+abstract class DataNode {
+  int rev = 0;
+
+  bool isNewer(DataNode other) => rev > other.rev;
+}
+
+abstract class ObservableNode {
+  List<ListenerNode> listeners;
+
+  void addListener(ListenerNode n) {
+    if (listeners == null) {
+      listeners = [];
+    }
+    listeners.add(n);
+  }
+
+  void removeListener(ListenerNode n) {
+    listeners[listeners.indexOf(n)] = listeners.last;
+    listeners.removeLast();
+  }
+
+  void invalidateListeners() {
+    for (var i = 0; i < listeners.length; i++) {
+      listeners[i].invalidate();
+    }
+  }
+}
+
+abstract class ListenerNode {
+  List<ObservableNode> dependencies;
+
+  void listen(ObservableNode n) {
+    if (dependencies == null) {
+      dependencies = [];
+    }
+    dependencies.add(n);
+    n.addListener(this);
+  }
+
+  void resetDependencies() {
+    if (dependencies != null) {
+      for (var i = 0; i < dependencies.length; i++) {
+        dependencies[i].removeListener(this);
+      }
+    }
+  }
+
+  void invalidate();
+}
+
+abstract class StoreNode extends DataNode with ObservableNode {
+  void commit() {
+    rev = scheduler.clock;
+    if (listeners != null) {
+      for (var i = 0; i < listeners.length; i++) {
+        listeners[i].invalidate();
+      }
+    }
+  }
+}
+
+abstract class CacheNode extends DataNode with ObservableNode, ListenerNode {
+  bool isDirty = false;
+
+  void invalidate() {
+    if (!isDirty) {
+      isDirty = true;
+
+      invalidateListeners();
+      resetDependencies();
+
+      listeners = null;
+      dependencies = null;
+      invalidated();
+    }
+  }
+
+  void invalidated() {}
+}
