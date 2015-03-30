dfc104654f406d98de9e069044c3df6de32ed1ed
Boris Kaul <localvoid@gmail.com>
2015-03-30 23:39:10 +0600

    Add comments

--- lib/src/component.dart
+++ lib/src/component.dart
@@ -11,31 +11,66 @@ import 'vcontext.dart';
 import 'vnode.dart';
 import 'env.dart';
 
-typedef Component componentConstructor([dynamic data, List<VNode> children, Component parent]);
-
+/// uix [Component] is a basic block to build user interfaces.
+///
+/// Example:
+///
+/// ```dart
+/// @ComponentMeta()
+/// class Button extends Component<String> {
+///   updateView() {
+///     updateRoot(vRoot(type: 'Button')(data));
+///   }
+/// }
+/// ```
 abstract class Component<P> extends RevisionedNode with StreamListenerNode implements VContext {
+  /// Flag indicating that [Component] is dirty and should be updated.
   static const int dirtyFlag = 1;
+
+  /// Flag indicating that [Component] is attached to the html document.
   static const int attachedFlag = 1 << 1;
+
+  /// Flag indicating that [Component]s [element] in svg namespace.
   static const int svgFlag = 1 << 2;
+
+  /// Flag indicating that [Component] in the process of mounting on top of existing html nodes.
   static const int mountingFlag = 1 << 3;
+
+  /// Set of flags indicating that [Component]s should be updated.
   static const int shouldUpdateViewFlags = dirtyFlag | attachedFlag;
 
+  /// Tag name of the root [element].
   final String tag = 'div';
 
+  /// Flags.
   int flags = dirtyFlag;
+
+  /// Depth relative to other [Component]s.
+  ///
+  /// It is used as a priority for dom write tasks. [Component]s with the
+  /// lowest depth have highest priority.
   int depth = 0;
+
+  /// Reference to the [html.Element].
   html.Element element;
-  Component _parent;
+
+  /// Protected property that contains data input.
   P data_;
+
+  /// Protected property that contains children input.
   List<VNode> children_;
+
+  Component _parent;
   VNode _root;
 
+  /// Parent [Component].
   Component get parent => _parent;
   set parent(Component newParent) {
     _parent = newParent;
     depth = newParent == null ? 0 : newParent.depth + 1;
   }
 
+  /// Data input.
   P get data => data_;
   set data(P newData) {
     if (data_ != newData) {
@@ -44,6 +79,7 @@ abstract class Component<P> extends RevisionedNode with StreamListenerNode imple
     }
   }
 
+  /// Children input.
   List<VNode> get children => children_;
   set children(List<VNode> newChildren) {
     if (!identical(children_, newChildren)) {
@@ -52,24 +88,54 @@ abstract class Component<P> extends RevisionedNode with StreamListenerNode imple
     }
   }
 
+  /// Reference to the root virtual dom node.
   VNode get root => _root;
 
+  /// [Component] is dirty and should be updated.
   bool get isDirty => (flags & dirtyFlag) != 0;
+
+  /// [Component] is attached to the html document.
   bool get isAttached => (flags & attachedFlag) != 0;
+
+  /// [Component]s [element] in svg namespace.
   bool get isSvg => (flags & svgFlag) != 0;
+
+  /// [Component] in the process of mounting on top of existing html nodes.
   bool get isMounting => (flags & mountingFlag) != 0;
 
+  /// Lifecycle method [create].
+  ///
+  /// [create] method should create root [element] of the [Component].
+  ///
+  /// Invoked during the [Scheduler] writeDom phase.
   void create() {
     element = html.document.createElement(tag);
   }
 
+  /// Lifecycle method [mount].
+  ///
+  /// [mount] method should mount [Component] on top of [e] element.
+  ///
+  /// Invoked during the [Scheduler] writeDom phase.
   void mount(html.Element e) {
     flags |= mountingFlag;
     element = e;
   }
 
+  /// Lifecycle method [init].
+  ///
+  /// Initialize component. This method is called after [create],
+  /// or [mount] methods.
+  ///
+  /// Invoked during the [Scheduler] writeDom phase.
   void init() {}
 
+  /// Lifecycle method [update].
+  ///
+  /// This method updates [Component] state, and if state is changed,
+  /// it will update view.
+  ///
+  /// Invoked during the [Scheduler] writeDom phase.
   void update([_]) {
     if ((flags & shouldUpdateViewFlags) == shouldUpdateViewFlags) {
       Future future;
@@ -90,9 +156,22 @@ abstract class Component<P> extends RevisionedNode with StreamListenerNode imple
     updated();
   }
 
+  /// Lifecycle method [updateState].
+  ///
+  /// update internal state, it should return [:bool:] value that indicates
+  /// if the state is changed.
+  ///
+  /// Invoked during the [Scheduler] writeDom phase.
   bool updateState() => true;
+
+  /// Lifecycle method [updateState].
+  ///
+  /// update view.
+  ///
+  /// Invoked during the [Scheduler] writeDom phase.
   Future updateView();
 
+  /// update internal tree using virtual dom representation.
   void updateRoot(VNode n) {
     assert(n != null);
     if (_root == null) {
@@ -110,6 +189,7 @@ abstract class Component<P> extends RevisionedNode with StreamListenerNode imple
     _root = n;
   }
 
+  /// invalidate
   void invalidate([_]) {
     if ((flags & dirtyFlag) == 0) {
       flags |= dirtyFlag;
@@ -129,11 +209,27 @@ abstract class Component<P> extends RevisionedNode with StreamListenerNode imple
     }
   }
 
+  /// Lifecycle method [invalidated].
   void invalidated() {}
+
+  /// Lifecycle method [updated].
+  ///
+  /// Invoked during the [Scheduler] writeDom phase.
   void updated() {}
 
+  /// Lifecycle method [attached].
+  ///
+  /// Invoked during the [Scheduler] writeDom phase.
   void attached() {}
+
+  /// Lifecycle method [detached].
+  ///
+  /// Invoked during the [Scheduler] writeDom phase.
   void detached() {}
+
+  /// Lifecycle method [disposed].
+  ///
+  /// Invoked during the [Scheduler] writeDom phase.
   void disposed() {}
 
   void dispose() {
@@ -234,6 +330,7 @@ abstract class Component<P> extends RevisionedNode with StreamListenerNode imple
   }
 }
 
+/// [Component] with a root element in Svg namespace.
 abstract class SvgComponent<P> extends Component<P> {
   final String tag = 'svg';
   int flags = Component.dirtyFlag | Component.svgFlag;
@@ -242,3 +339,9 @@ abstract class SvgComponent<P> extends Component<P> {
     element = html.document.createElementNS('http://www.w3.org/2000/svg', tag);
   }
 }
+
+/// [Component] constructor method type.
+///
+/// It will be removed when constructor closurization is implemented in Dart.
+/// https://github.com/dart-lang/dart_enhancement_proposals/blob/master/Accepted/0003%20-%20Generalized%20Tear-offs/proposal.md
+typedef Component componentConstructor([dynamic data, List<VNode> children, Component parent]);
