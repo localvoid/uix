604c33456bdb6de734af7b6d3bcff5f5d0879bf5
Boris Kaul <localvoid@gmail.com>
2015-04-02 12:19:27 +0600

    Fix container library name

--- lib/src/container.dart
+++ lib/src/container.dart
@@ -2,7 +2,7 @@
 // details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-library uix.src.vcontext;
+library uix.src.container;
 
 import 'vcontext.dart';
 import 'vnode.dart';
